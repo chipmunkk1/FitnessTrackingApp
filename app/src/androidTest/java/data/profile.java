30738419f3f7a903a0cd2f66afc0d2e7ccb9bbc6
karawan <banat811@gmail.com>
2022-09-29 11:39:55 +0300

    profile not completed ...

---
+++ app/src/androidTest/java/data/profile.java
@@ -0,0 +1,41 @@
+package data;
+
+public class profile {
+    private double weight;
+    private int length;
+    private int age;
+    private String gender;
+    private int active;   //how much hes active in a day (1: low , 2:meduim , 3: high)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
