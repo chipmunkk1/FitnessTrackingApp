0a275cc9bc38ea512bda8f4cdfa29df9cac668df
Chipmunkk1 <rohi.soul@gmail.com>
2022-10-04 11:37:59 +0300

    designing the client activity...

--- app/src/main/java/rao/rohi/rohirealfinalproject/ClientDetail.java
+++ app/src/main/java/rao/rohi/rohirealfinalproject/ClientDetail.java
@@ -2,13 +2,57 @@ package rao.rohi.rohirealfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ClientDetail extends AppCompatActivity {
+    private TextView txtView;
+    private TextInputEditText etWeight,etLength,etAge,etActive;
+    private CheckBox CheckBoxFemale,CheckBoxMale;
+    private Button btnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_detail);
+
+        CheckBoxFemale=findViewById(R.id.CheckBoxFemale);
+        CheckBoxMale=findViewById(R.id.CheckBoxMale);
+        txtView=findViewById(R.id.txtView);
+        etWeight=findViewById(R.id.etWeight);
+        etLength=findViewById(R.id.etLength);
+        etAge=findViewById(R.id.etAge);
+        etActive=findViewById(R.id.etActive);
+        btnCheck=findViewById(R.id.btnCheck);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ClientDetail.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
